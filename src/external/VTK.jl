928a164a17fe8b36ad205b2672e6608500e94747
Deeptendu Santra <santradibbo@gmail.com>
2020-12-17 19:43:58 +0530

    Fixed Docstring and functions

--- src/external/VTK.jl
+++ src/external/VTK.jl
@@ -24,7 +24,7 @@ The density is stored in 3 submatrices : Real, Real components of Fourier, Imagi
 components of Fourier.
 """
 function DensityToVtk(VTKFileName::String, ρ::RealFourierArray{T, <: AbstractArray{T, 3}, 
-                                                 <: AbstractArray{Complex{T}, 3}}) where T<:Real
+                                             <: AbstractArray{Complex{T}, 3}}) where T<:Real
 
     out = vtk_write_array(VTKFileName,(ρ.real, Base.getproperty.(ρ.fourier, :re),
                           Base.getproperty.(ρ.fourier, :im)), 
@@ -44,7 +44,7 @@ function EigenValuesToVTK(VTKFileName::String, Eigenvalues::Array{Array{T,1},1})
 end
 
 """
-    WavesTOVTK(VTKFIleName::String, wave)   
+    WavesToVTK(VTKFIleName::String, wave)   
 
 Stores the wave values in a VTK image file and returns the name of the file.
 If the sizes of wave vectors for each each value is not equal, the wave matrix
@@ -52,25 +52,28 @@ will have size = (e, w, n) where e = No of eigen values, w = max no of values fo
 each eigen value, n =  no of Kpoints). The matrix is stored as two submatrix,
 one containing the real part and the other containing the imaginary part.
 """
-function WavesTOVTK(VTKFIleName::String, wave)
+function WavesToVTK(VTKFileName::String, wave)
 
-    size = (size(wave[1])[2], maximum(x->size(x)[1], wave), length(wave))
-    wave_mat = zeros(Complex, size...)
+    mat_size = (maximum(x->size(x)[1], wave), size(wave[1])[2], length(wave))
+    wave_mat = zeros(ComplexF64, mat_size)
     @views for i = 1:length(wave)
-        wave_mat[:,:,i] = wave[i]
+        (x, y) = size(wave[i])
+        for m in 1:x, n in 1:y
+            wave_mat[m,n,i] = wave[i][m, n]
+        end
     end
     out = vtk_write_array(VTKFileName, (Base.getproperty.(wave_mat, :re), 
-            Base.getproperty.(wave_mat, :im)), "Wave")
+            Base.getproperty.(wave_mat, :im)), ("Wave Real", "Wave Imag."))
     return out[1]
 end
 """
     OccupationToVTK(VTKFileName::String, occupation)
-    
+
 Stores the occupation values in a VTK image file and returns the file name.
 """
 function OccupationToVTK(VTKFileName::String, occupation::Array{Array{T,1},1}) where T<:Real
    
-    occupation_mat = hcat(occupation)
+    occupation_mat = hcat(occupation...)
     out = vtk_write_array(VTKFileName, occupation_mat, "Occupation")
     return out[1]
 end
